7a9ff12821c145797434973d00a9829ed7891ff9
PHUMIN SROYTHO <phumin.sroytho.work@gmail.com>
2024-03-02 19:26:22 +0700

    third

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,42 @@
-import java.util.regex.Pattern;
-
 public class Main {
-    public static final String ONLY_DIGITS_REGEX = "\\d+$";
-    public static final String LOG_FORMAT = "This input %s only digits";
-    public static final String IS = "is";
-    public static final String IS_NOT = "is not";
-
     public static void main(String[] args) {
-        String input = "123456";
-        System.out.printf((LOG_FORMAT), isOnlyDigits(input) ? IS : IS_NOT);
+        int inputNumRows = 8;
+
+        printPascalTriangle(inputNumRows);
     }
 
-    public static boolean isOnlyDigits(String input) {
-        return Pattern.matches(ONLY_DIGITS_REGEX, input);
+    public static void printPascalTriangle(int numRows) {
+        int maxWidth = numRows * 2;
+
+        for (int i = 0; i < numRows; i++) {
+            int[] row = generatePascalRow(i);
+            int padding = (maxWidth - (row.length * 2)) / 2;
+            // Print for space
+            for (int p = 0; p < padding; p++) {
+                System.out.print(" ");
+            }
+            // Print for triangle
+            for (int k : row) {
+                System.out.print(k + " ");
+            }
+            System.out.println();
+        }
     }
+
+    public static int[] generatePascalRow(int rowNum) {
+        int[] row = new int[rowNum + 1];
+        row[0] = 1;
+
+        for (int i = 1; i <= rowNum; i++) {
+            /*
+                Formula nextValue = valueInside*((n-k+1)/k)
+                n = rowNumber
+                k, i = position
+             */
+            row[i] = row[i - 1] * (rowNum - i + 1) / i;
+        }
+
+        return row;
+    }
+
 }
